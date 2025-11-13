5d5ade8e26c29147a4d853e69872fe61d2836886
Alexandr Pershin <aleks98_06@mail.ru>
2025-11-13 22:40:43 +0700

    фикс линтер

---
+++ src/test/java/hexlet/code/controller/TaskStatusControllerTest.java
@@ -0,0 +1,10 @@
+package hexlet.code.controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskStatusControllerTest {
+}
