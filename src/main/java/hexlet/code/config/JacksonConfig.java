3607efcaf165f37a399f7b420ba132b8877bf6ef
Alexandr Pershin <aleks98_06@mail.ru>
2025-11-16 15:12:27 +0700

    фикс дат 2

--- src/main/java/hexlet/code/config/JacksonConfig.java
+++ src/main/java/hexlet/code/config/JacksonConfig.java
@@ -1,17 +1,10 @@
 package hexlet.code.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JacksonConfig {
@@ -19,13 +12,10 @@ public class JacksonConfig {
     @Bean
     Jackson2ObjectMapperBuilder objectMapperBuilder() {
         var builder = new Jackson2ObjectMapperBuilder();
+
         builder.serializationInclusion(JsonInclude.Include.NON_NULL)
-                .modules(new JavaTimeModule(), new JsonNullableModule())
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .deserializerByType(LocalDateTime.class,
-                        new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")))
-                .serializerByType(LocalDateTime.class,
-                        new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
+                .modulesToInstall(new JsonNullableModule());
+
         return builder;
     }
 }
